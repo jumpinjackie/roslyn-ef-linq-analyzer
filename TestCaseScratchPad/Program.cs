f03ca29c4c99010511f0b7928ac0712da83281a2
Jackie Ng <jumpinjackie@gmail.com>
2015-07-30 02:37:31 +1000

    More about the scratch pad source file.

--- TestCaseScratchPad/Program.cs
+++ TestCaseScratchPad/Program.cs
@@ -1,7 +1,9 @@
 ﻿/*
 You can use this file as a "scratch area" for building sources for analyzer test cases.
 
-Having it here ensures the source code for your analyzer test case is at least synatically sound
+Having it here ensures the source code for your analyzer test case is at least synatically sound by just
+simply pasting the contents of the test case right here (and commenting out the default entry point
+if required) and seeing if it compiles
  */
 
 namespace TestCaseScratchPad
